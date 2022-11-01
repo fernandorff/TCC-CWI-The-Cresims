4ebb1fe4e8d772ddd5001e3247e61d04ed34e711
fernandorff92 <fernandorff92@gmail.com>
2022-10-31 21:58:17 -0300

    animations

--- src/animations/showerAnimation.js
+++ src/animations/showerAnimation.js
@@ -24,14 +24,4 @@ ${a(r())}${a(r())} LL ${a(r())}${a(r())}
 
   await new Promise((resolve) => setTimeout(resolve, 500));
   console.clear();
-
-  console.log(`
-${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}
-${a(r())}${a(r())},,,${a(r())}${a(r())}
-${a(r())}<(u_u)>${a(r())}
-${a(r())}${a(r())} |  ${a(r())}
-${a(r())}${a(r())} LL ${a(r())}${a(r())}
-`);
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  console.clear();
 }
