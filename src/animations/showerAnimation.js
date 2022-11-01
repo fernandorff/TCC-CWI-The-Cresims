d2e53f60d0a7fd0168dd6c66e3b03246cca16ec7
fernandorff92 <fernandorff92@gmail.com>
2022-10-31 21:15:42 -0300

    criando animacoes

---
+++ src/animations/showerAnimation.js
@@ -0,0 +1,37 @@
+function a(x) {
+  switch (x) {
+    case 1:
+      return `、`;
+    case 2:
+      return `ヽ`;
+    case 3:
+      return `｀`;
+  }
+}
+
+function r() {
+  return Math.ceil(Math.random() * 3);
+}
+
+while (true) {
+  console.log(`
+${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}
+${a(r())}${a(r())},,,${a(r())}${a(r())}
+${a(r())}<(u_u)>${a(r())}
+${a(r())}${a(r())} |${a(r())}${a(r())}
+${a(r())}${a(r())} LL ${a(r())}${a(r())}
+`);
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  console.clear();
+
+  console.log(`
+${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}${a(r())}
+${a(r())}${a(r())},,,${a(r())}${a(r())}
+${a(r())}<(u_u)>${a(r())}
+${a(r())}${a(r())} |  ${a(r())}
+${a(r())}${a(r())} LL ${a(r())}${a(r())}
+`);
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  console.clear();
+}
