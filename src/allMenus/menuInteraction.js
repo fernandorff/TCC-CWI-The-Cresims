74808e5b9363364f0b8bf51a861d746f7f0b534f
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-01 18:33:13 -0300

    Logica inicial da interação adicionado

--- src/allMenus/menuInteraction.js
+++ src/allMenus/menuInteraction.js
@@ -1,4 +1,10 @@
 import { getCharacter } from "../crud/character.js"
+import { useQuestion } from "../services/question/use-question.js";
+import { 
+    getLevelInteraction,
+    listInteraction,
+    interaction
+} from "../characterActions/interaction.js";
 
 export const menuInteraction = async (character) => {
     while (true) {
@@ -11,40 +17,72 @@ export const menuInteraction = async (character) => {
             const newCharacter = createRelation(character, characterInteraction)
             const characterSecond = createRelation(characterInteraction, character)
 
-            return selectInteraction(newCharacter, characterSecond);
+            return await selectInteraction(newCharacter, characterSecond.id);
         } else {
             console.log("Informe um id de personagem diferente do seu")
         }
     }
 }
 
-const selectInteraction = (character, characterInterac) => {
-    console.table(character)
-    console.table(characterInterac)
+const selectInteraction = async (character, idCharacter) => {
+    while (true) {
+        console.log(`Escolha uma das opções de interação abaixo:`)
+
+        const points = pointsInteraction(character, idCharacter);
+        const level = getLevelInteraction(points);
+        const list = await listInteraction(level);
+
+        while (true) {
+            showInteractions(list)
+            console.log(list[0])
+            const input = await useQuestion(`Id da interação escolhida: `)
+
+            if (Number(input)) {
+                return interaction(character, idCharacter, list[input - 1])
+            }
+        }
+    }
 }
 
 const createRelation = (character, characterSecondary) => {
-    const relation = getRelation(character, characterSecondary)
+    const relation = getRelation(character, characterSecondary.id)
 
     if (!relation) {
         character.relationship.push({
             id: characterSecondary.id,
             name: characterSecondary.name,
-            pontos: 0
+            level: 0
         })
     }
 
     return character
 }
 
-const getRelation = (character, characterSecondary) => {
+const getRelation = (character, idCharacter) => {
     const listRelation = character.relationship;
     return listRelation.find((charac) => 
-        charac.id == characterSecondary.id
+        charac.id == idCharacter
     );
 }
 
+const pointsInteraction = (character, idCharacter) => {
+    const relation = getRelation(character, idCharacter)
+    return relation.level
+}
+
+const showInteractions = (list) => {
+    let cont = 1;
+    for (const interaction of list) {
+        console.table({
+            ...interaction,
+            id: cont
+        })
+        cont++;
+    }
+}
+
 menuInteraction({
+    id: 1,
     name: 'Fulano',
     aspiration: 'JOGOS',
     cresceleons: 10500,
--- src/characterActions/interaction.js
+++ src/characterActions/interaction.js
@@ -1,1 +1,34 @@
-console.log("Em construção ainda")
+import { getStorage } from "../crud/storage.js"
+import { interactionsDataApi } from "../services/api/api.js"
+
+export const interaction = async (character, idCharacter, interaction) => {
+    const list = await getStorage()
+    const characterSecondary = list.find(char => char.id == idCharacter)
+    console.log(`A interação "${interaction.interacao}" foi realada com sucesso`)
+}
+
+export const listInteraction = async (level) => {
+    const list = await interactionsDataApi();
+
+    switch (level) {
+        case "INIMIZADE":
+            return [...list.INIMIZADE, ...list.NEUTRO];
+        case "NEUTRO":
+            return [...list.NEUTRO];
+        case "AMIZADE":
+            return [...list.AMIZADE, list.NEUTRO]
+        default:
+            return [...list.AMOR, ...list.AMIZADE, list.NEUTRO]
+    }
+}
+
+export const getLevelInteraction = (points) => {
+    if (points < 0) {
+        return "INIMIZADE";
+    } else if (points < 10) {
+        return "NEUTRO";
+    } else if (points < 25) {
+        return "AMIZADE";
+    }
+    return "AMOR";
+}
