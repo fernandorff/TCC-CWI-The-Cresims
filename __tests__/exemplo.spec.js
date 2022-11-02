84d327f8760447d6d29b76392e2f8859a8c80ccd
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-11-02 16:11:05 -0300

    Mudando a ordem dos testes

--- __tests__/exemplo.spec.js
+++ __tests__/exemplo.spec.js
@@ -71,131 +71,7 @@ beforeEach(() => {
   productChoice = product[0];
 });
 
-describe("5 - Relacionamentos", () => {
-  it("Deve evoluir o relacionamento de dois Cresims para AMIZADE", () => {
-    const objInteraction = {
-      "id": 3,
-      "interacao": "Elogiar",
-      "pontos": 4,
-      "energia": 1
-    };
-    
-    let newCharacter, newCharacter_02;
-    for (let cont = 0; cont < 4; cont++) {
-      [newCharacter, newCharacter_02] = interaction(
-        character, character_02, objInteraction
-      );
-    }
-
-    const pointsCharacter = newCharacter.relationship[0].level;
-    const pointsCharacter_02 = newCharacter_02.relationship[0].level;
-
-    const levelCharacter = getLevelInteraction(pointsCharacter);
-    const levelCharacter_02 = getLevelInteraction(pointsCharacter_02);
-
-    expect(levelCharacter).toBe("AMIZADE");
-    expect(levelCharacter_02).toBe("AMIZADE");
-  })
-
-  it("Deve recuar o relacionamento de dois Cresims para INIMIZADE", () => {
-    const objInteraction = {
-      "id": 6,
-      "interacao": "Criticar",
-      "pontos": -3,
-      "energia": 2
-    };
-    
-    const [newCharacter, newCharacter_02] = interaction(
-      character, character_02, objInteraction
-    );
-
-    const pointsCharacter = newCharacter.relationship[0].level;
-    const pointsCharacter_02 = newCharacter_02.relationship[0].level;
-
-    const levelCharacter = getLevelInteraction(pointsCharacter);
-    const levelCharacter_02 = getLevelInteraction(pointsCharacter_02);
-
-    expect(levelCharacter).toBe("INIMIZADE");
-    expect(levelCharacter_02).toBe("INIMIZADE");
-  })
-
-  it("Deve descontar os pontos de energia em uma interação entre dois Cresims", () => {
-    const objInteraction = {
-      "id": 4,
-      "interacao": "Conversar",
-      "pontos": 2,
-      "energia": 2
-    };
-    
-    const [newCharacter, newCharacter_02] = interaction(
-      character, character_02, objInteraction
-    );
-
-    const energyCharacter = newCharacter.energy;
-    const energyCharacter_02 = newCharacter_02.energy;
-
-    const energyExpect = 30
-    const energyExpect_02 = 31
-
-    expect(energyCharacter).toBe(energyExpect);
-    expect(energyCharacter_02).toBe(energyExpect_02);
-  })
-})
-
-describe("4 - Trabalho", () => {
-  it("Deve perder os pontos de energia ao trabalhar uma jornada padrão", async () => {
-    const characterWork = await work(character);
-
-    const energy = characterWork.energy;
-    const energyExpected = 22;
-
-    expect(energy).toBe(energyExpected);
-  });
-
-  it("Deve receber o salario do dia ao trabalhar uma jornda padrão", async () => {
-    const characterWork = await work(character);
-
-    const salary = characterWork.employee.salary;
-    const salaryExpected = 160.0;
-
-    expect(salary).toBe(salaryExpected);
-  });
-
-  it("Deve receber o salario equivalente quando começar a trabalhar com os pontos de energia menores que 10", async () => {
-    character.energy = 9;
-    const characterWork = await work(character);
-
-    const salary = characterWork.employee.salary;
-    const salaryExpected = 107.2;
-
-    expect(salary.toFixed(1)).toBe(salaryExpected.toFixed(1));
-  });
-
-  it("Deve receber o salario equivalente quando começar a trabalhar com os pontos de energia menores que 10 e pontos de higiene menores que 4", async () => {
-    const characterCopy = { ...character };
-
-    characterCopy.energy = 10;
-    characterCopy.hygiene = 3;
-
-    const characterWork = await work(characterCopy);
-
-    const salary = characterWork.employee.salary;
-    const salaryExpected = 110.9;
-
-    expect(salary).toBe(salaryExpected);
-  });
-
-  it("Deve validar para que o Cresim não consiga começar a trabalhar com os pontos de energia menores que 4", async () => {
-    const ENERGY_CRESIM = 3;
-
-    const boolWork = isWork(ENERGY_CRESIM, character.employee);
-    const boolWorkExpected = false;
-
-    expect(boolWork).toBe(boolWorkExpected);
-  });
-});
-
-describe("2 - Habilidades e aspirações", () => {
+describe("3 - Habilidades e aspirações", () => {
   it("Deve conseguir comprar um item de habilidade", () => {
     character.cresceleons += 2000;
     const characterBuys = buyProductItens(character, productChoice);
@@ -282,6 +158,130 @@ describe("2 - Habilidades e aspirações", () => {
     expect(levelSkill).toBe(levelSkillExpected);
   });
 });
+
+describe("4 - Trabalho", () => {
+  it("Deve perder os pontos de energia ao trabalhar uma jornada padrão", async () => {
+    const characterWork = await work(character);
+
+    const energy = characterWork.energy;
+    const energyExpected = 22;
+
+    expect(energy).toBe(energyExpected);
+  });
+
+  it("Deve receber o salario do dia ao trabalhar uma jornda padrão", async () => {
+    const characterWork = await work(character);
+
+    const salary = characterWork.employee.salary;
+    const salaryExpected = 160.0;
+
+    expect(salary).toBe(salaryExpected);
+  });
+
+  it("Deve receber o salario equivalente quando começar a trabalhar com os pontos de energia menores que 10", async () => {
+    character.energy = 9;
+    const characterWork = await work(character);
+
+    const salary = characterWork.employee.salary;
+    const salaryExpected = 107.2;
+
+    expect(salary.toFixed(1)).toBe(salaryExpected.toFixed(1));
+  });
+
+  it("Deve receber o salario equivalente quando começar a trabalhar com os pontos de energia menores que 10 e pontos de higiene menores que 4", async () => {
+    const characterCopy = { ...character };
+
+    characterCopy.energy = 10;
+    characterCopy.hygiene = 3;
+
+    const characterWork = await work(characterCopy);
+
+    const salary = characterWork.employee.salary;
+    const salaryExpected = 110.9;
+
+    expect(salary).toBe(salaryExpected);
+  });
+
+  it("Deve validar para que o Cresim não consiga começar a trabalhar com os pontos de energia menores que 4", async () => {
+    const ENERGY_CRESIM = 3;
+
+    const boolWork = isWork(ENERGY_CRESIM, character.employee);
+    const boolWorkExpected = false;
+
+    expect(boolWork).toBe(boolWorkExpected);
+  });
+});
+
+describe("5 - Relacionamentos", () => {
+  it("Deve evoluir o relacionamento de dois Cresims para AMIZADE", () => {
+    const objInteraction = {
+      "id": 3,
+      "interacao": "Elogiar",
+      "pontos": 4,
+      "energia": 1
+    };
+    
+    let newCharacter, newCharacter_02;
+    for (let cont = 0; cont < 4; cont++) {
+      [newCharacter, newCharacter_02] = interaction(
+        character, character_02, objInteraction
+      );
+    }
+
+    const pointsCharacter = newCharacter.relationship[0].level;
+    const pointsCharacter_02 = newCharacter_02.relationship[0].level;
+
+    const levelCharacter = getLevelInteraction(pointsCharacter);
+    const levelCharacter_02 = getLevelInteraction(pointsCharacter_02);
+
+    expect(levelCharacter).toBe("AMIZADE");
+    expect(levelCharacter_02).toBe("AMIZADE");
+  })
+
+  it("Deve recuar o relacionamento de dois Cresims para INIMIZADE", () => {
+    const objInteraction = {
+      "id": 6,
+      "interacao": "Criticar",
+      "pontos": -3,
+      "energia": 2
+    };
+    
+    const [newCharacter, newCharacter_02] = interaction(
+      character, character_02, objInteraction
+    );
+
+    const pointsCharacter = newCharacter.relationship[0].level;
+    const pointsCharacter_02 = newCharacter_02.relationship[0].level;
+
+    const levelCharacter = getLevelInteraction(pointsCharacter);
+    const levelCharacter_02 = getLevelInteraction(pointsCharacter_02);
+
+    expect(levelCharacter).toBe("INIMIZADE");
+    expect(levelCharacter_02).toBe("INIMIZADE");
+  })
+
+  it("Deve descontar os pontos de energia em uma interação entre dois Cresims", () => {
+    const objInteraction = {
+      "id": 4,
+      "interacao": "Conversar",
+      "pontos": 2,
+      "energia": 2
+    };
+    
+    const [newCharacter, newCharacter_02] = interaction(
+      character, character_02, objInteraction
+    );
+
+    const energyCharacter = newCharacter.energy;
+    const energyCharacter_02 = newCharacter_02.energy;
+
+    const energyExpect = 30
+    const energyExpect_02 = 31
+
+    expect(energyCharacter).toBe(energyExpect);
+    expect(energyCharacter_02).toBe(energyExpect_02);
+  })
+})
 
 describe("6 - Cheats", () => {
   it("Deve conseguir aplicar o cheat SORTENAVIDA e receber as recompensas", async () => {
