48a6ccfeded9d583ed802f6d88a8cabeaf0f4105
ramonmodesto16 <ramonmodesto16@protonmail.com>
2022-11-02 19:00:28 -0300

    Refatoração

--- src/allMenus/menuBuyItens.js
+++ src/allMenus/menuBuyItens.js
@@ -1,20 +1,16 @@
+import { animationBuyItens } from "../animations/animationBuyItens.js";
 import { animationTimeCount } from "../animations/animationTimeCount.js";
 import { buyProductItens } from "../characterActions/skillAspiration.js";
 import { itensSkillDataApi } from "../services/api/api.js";
 import { useQuestion } from "../services/question/use-question.js";
 import { menuAbilitys } from "./menuAbilitys.js";
-import { theCresimsLogo } from "./theCresimsLogo.js";
 
 export const menuBuyItens = async (character) => {
   const TIME = 3000;
   const response = await itensSkillDataApi();
   const skillChoice = await skillChoiceProduct();
   const product = await productChoice(response, skillChoice);
-  const characterBuys = buyProductItens(
-    character,
-    product,
-    skillChoice.toUpperCase()
-  );
+  const characterBuys = buyProductItens( character, product, skillChoice.toUpperCase());
 
   if (characterBuys) {
     return characterBuys;
@@ -29,33 +25,17 @@ export const productChoice = async (response, skillChoice) => {
   const listItensSkill = response[skillChoice.toUpperCase()];
 
   listItensSkill.forEach((product) => {
-    console.log(product.id + " - " + product.nome);
+    console.log(product.id + ". " + product.nome);
   });
+
+  console.log('X. Voltar ao menu principal \n')
 
   const choice = await useQuestion("Escolha uma produto");
   return listItensSkill[choice - 1];
 };
 
 export const skillChoiceProduct = async () => {
-  console.log(`
-${await theCresimsLogo()}
-  
- ___________________________________________________________
-| : : : : : : : : : : : : : : : : : : : : : : : : : : : : : |
-|: : : : : : : : :_________________________: : : : : : : : :|
-| : : : : : : : _)  :                   :  (_ : : : : : : : |
-|: : : : : : : )_ :   $ Lojas CWIanas $   : _( : : : : : : :|
-| : : :__________)_________________________(__________  : : |
-| /_/  '---------------------------------------------'  /_/ |
-|: | : |Música * Games * Jardinagem * Cozinha * Artes| : | :|
-| : : :|   ______    _    _________         ______   |: : : |
-|======| .' ,|,  '. /_/ .'         '. /_/ .'  ,|, '. |======|
-|      | |_';;;'__|  |  |   ,,,,    |  |  |__';;;'_| |      |
-|      | |_|-;-|__|     |  (⚆_⚆     |     |__|-;-|_| |      |
-|      | |________|     |   /|-$    |     |________| |      |
-|      |                |    LL     |                |      |
-l______|________________|           |________________|______|
-`);
+  await animationBuyItens()
   const choiceAspiration = menuAbilitys("Escolha um setor da loja: ");
 
   return choiceAspiration;
