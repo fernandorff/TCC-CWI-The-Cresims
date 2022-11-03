1aa4e1a9d8560e21389a76593be72d315bbb3e71
ramonmodesto16 <ramonmodesto16@protonmail.com>
2022-11-03 12:55:51 -0300

    Adiconado cores no console log

--- src/animations/animationTimeCount.js
+++ src/animations/animationTimeCount.js
@@ -2,7 +2,7 @@ export const animationTimeCount = async (time, text) => {
     let count = 0;
     while (count <= time / 1000) {
       console.clear();
-      console.log(`${text} --> ${count} | ${time / 1000}`);
+      console.log(`\x1b[33m${text} --> ${count} | ${time / 1000}\x1b[0m`);
       count = timeCount(count);
     }
   };
