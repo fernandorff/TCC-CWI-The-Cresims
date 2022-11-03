23cb5e717dc4285da1981eae52bb225f58014110
fernandorff92 <fernandorff92@gmail.com>
2022-11-03 10:58:01 -0300

    finalizando animacoes falta implementar

--- src/animations/gardenTrainingAnim.js
+++ src/animations/gardenTrainingAnim.js
@@ -1,50 +1,205 @@
-console.log(`
-                                       
-                                         
-    .|.:.|.:.|.:.|.:.|.:.|.:
-    '.'.'.'.'.'.'.'.'.'.'.'.
-    '.'.'.'.'./////'.'.'.'.'
-    .'./////'.'.'.'.'/////'.
-    .:.|.:.|.:.|.:.|.:.|.:.|
+import { theCresimsLogo } from "../allMenus/theCresimsLogo.js";
 
-    .|.: ,,,, .:.|.:.|.:.|.:
-    '.' (⚆_⚆ '.'.'.'.'.'.'.
-    ' ⅃⅃─< | ./////'.'.'.'.'
-    .'./////'.'.'.'.'/////'.
-    .:.|.:.|.:.|.:.|.:.|.:.|
+export const gardenTrainingAnim = async (character, display) => {
+  if (display == true) {
+    console.clear();
 
-    .|.: ,,,, .:.|.:.|.:.|.:
-    '.' (⚆_⚆ '.'.'.'.'.'.'.
-    ' ⅃⅃─/ > ./////'.'.'.'.'
-    .'./////'.'.'.'.'/////'.
-    .:.|.:.|.:.|.:.|.:.|.:.|
-               ,,,,
-    .|.:.|.:. (⚆_⚆ |.:.|.:
-    '.'.'.' ⅃⅃─< | .'.'.'.'.
-    '.'.'.'.'./////'.'.'.'.'
-    .'.'.'.'.'.'.'.'/////'.'
-    .:.|.:.|.:.|.:.|.:.|.:.|
-
-               ,,,,
-    .|.:.|.:. (⚆_⚆ |.:.|.:
-    '.'.'.' ⅃⅃─/ > .'.'.'.'.
-    '.'.'.'.'./////'.'.'.'.'
-    .'.'.'.'.'.'.'.'/////'.'
-    .:.|.:.|.:.|.:.|.:.|.:.|
-
-    .|.:.|.:.|.:.|.:.|.:.|.:
-    '.'.'.'.'.'.'.'.'.'.'.'.
-    '.'.'.'.'./////'.'.'.'.'
-    .'.'.'.'.'.'.'.'/////'.'
-    .:.|.:.|.:.|.:.|.:.|.:.|
-
-          ,,,,    
-         (⚆_⚆   
-       ⅃⅃─< |   
-
-       ,,,,    
-       (⚆_⚆   
-     ⅃⅃─/ >  
+    console.log(`
+${await theCresimsLogo()}   
+          
+  ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ   
+ 
   
+  .|.: ,,,, .:.|.:.|.:.|.:
+  '.' (⚆_⚆  '.'.'.'.'.'.'.
+  ' ⅃⅃─< | ./////.'.'.'.'.
+  '.'/////'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+          
+           1 / 10
+          
+${character} está cuidando das plantinhas.
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
 
-        `);
+    console.log(`
+${await theCresimsLogo()}      
+          
+ ☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°
+ 
+ 
+  .|.: ,,,, .:.|.:.|.:.|.:
+  '.' (⚆_⚆  '.'.'.'.'.'.'.
+  ' ⅃⅃─/ > ./////.'.'.'.'.
+  '.'/////'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.| 
+                
+           2 / 10
+          
+${character} está cuidando das plantinhas.
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆  
+  
+ 
+  .|.: ,,,, .:.|.:.|.:.|.:
+  '.' (⚆_⚆  '.'.'.'.'.'.'.
+  ' ⅃⅃─< | ./////.'.'.'.'.
+  '.'/////'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+          
+           3 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆ 
+                    
+             ,,,,
+  .|.:.|.:. (⚆_⚆  .|.:.|.:
+  '.'.'.' ⅃⅃─/ > .'.'.'.'.
+  '.'.'.'.'./////.'.'.'.'.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|  
+          
+           4 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺
+                    
+             ,,,,
+  .|.:.|.:. (⚆_⚆  .|.:.|.:
+  '.'.'.' ⅃⅃─< | .'.'.'.'.
+  '.'.'.'.'./////.'.'.'.'.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+          
+           5 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}      
+          
+ ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡
+                    
+             ,,,,
+  .|.:.|.:. (⚆_⚆  .|.:.|.:
+  '.'.'.' ⅃⅃─/ > .'.'.'.'.
+  '.'.'.'.'./////.'.'.'.'.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|    
+          
+            6 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+  ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ
+
+                    
+  .|.:.|.:.|.:.|.:. ,,,, :
+  '.'.'.'.'.'.'.'. (⚆_⚆  .
+  '.'.'.'.'.'.'. ⅃⅃─< | '.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+          
+           7 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ☁︎-₊⋆⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°
+ 
+                    
+  .|.:.|.:.|.:.|.:. ,,,, :
+  '.'.'.'.'.'.'.'. (⚆_⚆  .
+  '.'.'.'.'.'.'. ⅃⅃─/ > '.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+                                  
+           8 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ⁺₊⋆ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆
+  
+ 
+  .|.:.|.:.|.:.|.:. ,,,, :
+  '.'.'.'.'.'.'.'. (⚆_⚆  .
+  '.'.'.'.'.'.'. ⅃⅃─< | '.
+  '.'.'.'.'.'.'.'.'/////'.
+  .:.|.:.|.:.|.:.|.:.|.:.|                
+          
+           9 / 10
+          
+${character} está cuidando das plantinhas.
+          
+          `);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.clear();
+
+    console.log(`
+${await theCresimsLogo()}  
+          
+ ☀︎ ⋆⁺₊⋆-ﾟ☁︎｡ﾟ☁︎｡ﾟ ﾟ☁︎｡ﾟ°☁︎-₊⋆⁺₊⋆
+ 
+            ,,,,
+  .|.:.|.: (⚆_⚆  :.|.:.|.:
+  '.'.'.'.' <|v⋆ .'.'.'.'.
+  '.'.'.'.'. LL '.'.'.'.'.
+  '.'.'.'.'.'.'.'.'.'.'.'.
+  .:.|.:.|.:.|.:.|.:.|.:.|
+           
+          10 / 10
+          
+${character} cuidou do jardim.
+          
+          `);
+  }
+};
+
+gardenTrainingAnim("Fernando", true);
