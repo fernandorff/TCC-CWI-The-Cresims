a7561b17342dc5092215e12b0224986792d99fbe
ramonmodesto16 <ramonmodesto16@protonmail.com>
2022-11-03 12:43:11 -0300

    Adicionado chamamento para as animações do menuTranning

--- src/allMenus/menuTranning.js
+++ src/allMenus/menuTranning.js
@@ -1,10 +1,14 @@
 
 import { animationTimeCount } from "../animations/animationTimeCount.js"
+import { cookTrainingAnim } from "../animations/cookTrainingAnim.js"
+import { gameTrainingAnim } from "../animations/gameTrainingAnim.js"
+import { gardenTrainingAnim } from "../animations/gardenTrainingAnim.js"
+import { musicTrainingAnim } from "../animations/musicTrainingAnim.js"
+import { paintTrainingAnim } from "../animations/paintTrainingAnim.js"
 import { cicleTrainCharacterProductPurchased } from "../characterActions/skillAspiration.js"
 import { itensSkillDataApi } from "../services/api/api.js"
 import { useQuestion } from "../services/question/use-question.js"
 
-const TIME_CICLE_TRAINNING = 8000
 const TIME = 3000
 
 export const menuTrainning = async (character) => {
@@ -13,7 +17,7 @@ export const menuTrainning = async (character) => {
         return character
     }
 
-    if(character.energy <= 6) {
+    if (character.energy <= 6) {
         animationTimeCount(TIME, 'Energia insuficiente para treinamento')
         return character
     }
@@ -21,7 +25,8 @@ export const menuTrainning = async (character) => {
     const choice = await choiceItensCresim(character.items)
     const response = await itensSkillDataApi()
     const choiceItem = getChoiceItem(response, choice)
-    animationTimeCount(TIME_CICLE_TRAINNING, 'Treinando')
+
+    printAnimateTranning(character, choiceItem.skillName)
 
     return cicleTrainCharacterProductPurchased(character, choiceItem, choiceItem.skillName)
 }
@@ -50,4 +55,24 @@ export const getChoiceItem = (response, choiceItem) => {
     })
 
     return choice
+}
+
+const printAnimateTranning = (character, skillName) => {
+    switch (skillName) {
+        case 'GASTRONOMIA':
+            cookTrainingAnim(character, true)
+            break
+        case 'PINTURA':
+            paintTrainingAnim(character, true)
+            break
+        case 'JOGOS':
+            gameTrainingAnim(character, true)
+            break
+        case 'JARDINAGEM':
+            gardenTrainingAnim(character, true)
+            break
+        case 'MUSICA':
+            musicTrainingAnim(character, true)
+            break
+    }
 }
